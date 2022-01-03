556ad462cc7be554223b2b2358dcbedcfa371e1a
Rock-Bautumn <3623@holbertonschool.com>
2022-01-03 16:16:38 -0600

    Adding task 3 in 0x00

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    digit = number % 10
+elif number < 0:
+    digit = number % -10
+else:
+    digit = 0
+if digit > 0:
+    digit = number % 10
+elif digit < 0:
+    digit = number % -10
+else:
+    digit = 0
+if digit > 5:
+    bit = "greater than 5"
+elif digit == 0:
+    bit = "is 0"
+else:
+    bit = "less than 6"
+print("Last digit of {:d} is {:d} and is {}".format(number, digit, bit))
