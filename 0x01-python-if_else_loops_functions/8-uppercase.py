c1d1b80a3f255a7ae0bbb4bee04318fb5f568a05
Rock-Bautumn <3623@holbertonschool.com>
2022-01-04 23:00:57 -0600

    Updating task 8 to do what the task is actually asking for

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 
-def isupper(c):
-    if ord(c) >= 65 and ord(c) <= 90:
-        return True
-    else:
-        return False
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("\n", end="")
