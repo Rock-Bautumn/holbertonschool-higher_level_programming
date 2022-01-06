71ec34aef4864d51c2fc154c5dbd0bfaa9793338
Rock-Bautumn <3623@holbertonschool.com>
2022-01-05 21:35:37 -0600

    Adding task 1 to 0x01B

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+size = len(sys.argv) - 1
+
+if size == 0:
+    print("{} arguments.".format(size))
+elif size == 1:
+    print("{} argument:".format(size))
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(size))
+    for i in range(1, size + 1):
+        print("{}: {}".format(i, sys.argv[i]))
