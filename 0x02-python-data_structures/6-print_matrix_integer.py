37e5097b65ae07135089d0cb6a396cefec15dc4d
Rock-Bautumn <3623@holbertonschool.com>
2022-01-06 22:10:53 -0600

    handling matrices that are not square

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,8 @@ def print_matrix_integer(matrix=[[]]):
         print("\n", end="")
     else:
         for x in range(len(matrix)):
-            for y in range(len(matrix)):
-                if y < len(matrix) - 1:
+            for y in range(len(matrix[x])):
+                if y < len(matrix[x]) - 1:
                     print("{:d}".format(matrix[x][y]), end=' ')
                 else:
                     print("{:d}".format(matrix[x][y]))
