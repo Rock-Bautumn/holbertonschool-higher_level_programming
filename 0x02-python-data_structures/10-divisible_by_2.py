f1a2e3a109dfe411ec1b08c49cd1ce1e84b8683d
Rock-Bautumn <3623@holbertonschool.com>
2022-01-06 23:20:45 -0600

    Using my_list intead of new_list

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -4,8 +4,8 @@
 def divisible_by_2(my_list=[]):
  
     new_list = []
-    for n in range(len(new_list)):
-        if my_list[n] % 2 == 0:
+    for n in my_list:
+        if n % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
