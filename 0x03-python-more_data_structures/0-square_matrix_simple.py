e99623383a6f646f9e73e429d8a9796a428d2d57
Rock-Bautumn <3623@holbertonschool.com>
2022-01-10 13:21:35 -0600

    Initial commit for 0x03 - Adding task 0 and readme

---
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for row in range(0, len(matrix)):
+        squared_matrix.append(list(map(lambda x : x**2, matrix[row])))
+    return squared_matrix
---
+++ 0x03-python-more_data_structures/README.md
@@ -0,0 +1,5 @@
+**0x03-python-more_data_structures**
+
+*More data structures than before?*
+
+Exploring the rest of the data structure types in python.
