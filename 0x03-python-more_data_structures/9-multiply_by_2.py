eff1ec642475377c18932262c00e094cd1562550
Rock-Bautumn <3623@holbertonschool.com>
2022-01-10 16:59:01 -0600

    Adding task 9 to 0x03

--- 0x03-python-more_data_structures/9-multiply_by_2.py
+++ 0x03-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 
 
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return a_dictionary
+
+
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    for x, y in new_dict.items():
+        update_dictionary(new_dict, x, y*2)
+    return new_dict
