c31578cb0fc1f775e3354f2734c18e42254cf19a
Rock-Bautumn <3623@holbertonschool.com>
2022-01-10 13:55:02 -0600

    Adding task 1 to 0x03, pep8 for task 0

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 
-
 def square_matrix_simple(matrix=[]):
     squared_matrix = []
     for row in range(0, len(matrix)):
-        squared_matrix.append(list(map(lambda x : x**2, matrix[row])))
+        squared_matrix.append(list(map(lambda x: x**2, matrix[row])))
     return squared_matrix
--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 
-
+def search_replace(my_list, search, replace):
+    edit_list = []
+    for n in range(len(my_list)):
+        if my_list[n] == search:
+            edit_list.append(replace)
+        else:
+            edit_list.append(my_list[n])
+    return edit_list
