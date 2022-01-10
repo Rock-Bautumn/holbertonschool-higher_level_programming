1dcf7488c7f7f894693d12732e4c75e75b2b7296
Rock-Bautumn <3623@holbertonschool.com>
2022-01-10 14:22:33 -0600

    Adding task 2 in 0x03

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 
-
+def uniq_add(my_list=[]):
+    sum = 0
+    s_list = sorted(my_list)
+    for x in range(len(my_list)):
+        if x == 0 or s_list[x] != s_list[x-1]:
+            sum += s_list[x]
+    return sum
