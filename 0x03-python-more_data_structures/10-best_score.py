5a73ed132b9f1a26fec18c7bbcc4fa94f6798b81
Rock-Bautumn <3623@holbertonschool.com>
2022-01-10 17:19:20 -0600

    Adding task 10 to 0x03

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 
-
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+#    print("dictionary has {:d} items".format(len(a_dictionary.values())))
+    max = None
+    for key, value in a_dictionary.items():
+        if max is None or max < value:
+            best = key
+            max = value
+    return best
