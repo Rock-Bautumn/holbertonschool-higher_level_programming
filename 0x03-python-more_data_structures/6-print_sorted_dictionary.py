76d3aeb0c3e859590835204f64fde9df2d4276c6
Rock-Bautumn <3623@holbertonschool.com>
2022-01-10 15:35:30 -0600

    Adding task 6

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 
-
+def print_sorted_dictionary(a_dictionary):
+    s_dict = sorted(a_dictionary)
+    for item in s_dict:
+        print("{}: {}".format(item, a_dictionary[item]))
