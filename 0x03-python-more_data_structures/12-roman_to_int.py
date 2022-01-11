a151449226491429b880b1e82034a8bc48a2e73a
Rock-Bautumn <3623@holbertonschool.com>
2022-01-10 18:02:06 -0600

    Adding task 12 to 0x03

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
 
-
+def roman_to_int(roman_string):
+    r_dict = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900,
+              'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
+    i = 0
+    while i < len(roman_string):
+        if (i + 1) < len(roman_string) and roman_string[i:i + 2] in r_dict:
+            sum += r_dict[roman_string[i:i + 2]]
+            i += 2
+        else:
+            sum += r_dict[roman_string[i]]
+            i += 1
+    return sum
