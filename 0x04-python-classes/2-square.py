068dbc5e74e180a62521dd7d6dad080ff9a34498
Rock-Bautumn <3623@holbertonschool.com>
2022-01-11 20:43:41 -0600

    Adding task 2 to 0x04

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""This file holds the Square class"""
 
 
+class Square():
+    """The Square is initialized below"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """This returns the area of the square"""
+    def area(self):
+        return self.__size**2
