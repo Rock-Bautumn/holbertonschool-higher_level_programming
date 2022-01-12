1663e76b294c85d3f7f5dfa858330dc18bdda472
Rock-Bautumn <3623@holbertonschool.com>
2022-01-11 20:14:34 -0600

    Adding task 0 in 0x04, pep8 says it's okay, googlepylintrc says indentation is bad

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This file holds the Square class"""
+
+
+class Square():
+    """The Square is initialized below"""
+    def __init__(self, size=0):
+        self.__size = size
