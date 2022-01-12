83d0a0d952fb091b4d8e5ab78d86a69d9912bf85
Rock-Bautumn <3623@holbertonschool.com>
2022-01-11 20:34:06 -0600

    Typo fix for TypeError message in task 1 of 0x04

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -6,7 +6,7 @@ class Square():
     """The Square is initialized below"""
     def __init__(self, size=0):
         if type(size) is not int:
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
