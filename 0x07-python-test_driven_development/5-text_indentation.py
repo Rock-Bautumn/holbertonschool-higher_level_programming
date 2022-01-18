865aa6de7d43ee1e991fa53033c45eadcd0b0a14
Rock-Bautumn <3623@holbertonschool.com>
2022-01-17 22:44:03 -0600

    Updating task 5 in 0x07 to discard spaces after new lines

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,11 +8,14 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     nl_array = [".", "?", ":"]
+    no_space = 1
     for i in range(len(text)):
-        if text[i] == " " and text[i - 1] in nl_array:
+        if text[i] == " " and no_space == 1:
             pass
         else:
             print(text[i], end="")
+            no_space = 0
         if text[i] in nl_array:
             print("\n", end="")
             print("\n", end="")
+            no_space = 1
