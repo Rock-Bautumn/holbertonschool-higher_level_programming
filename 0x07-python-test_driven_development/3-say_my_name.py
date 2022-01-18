8a8264316e8251c6e1597673ff3c22883789c6aa
Rock-Bautumn <3623@holbertonschool.com>
2022-01-17 19:27:18 -0600

    Adding files for task 2 in 0x07

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module has a function that prints a name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the name given.
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} ".format(first_name), end="")
+    if last_name != "":
+        print(last_name)
+    else:
+        print("")
