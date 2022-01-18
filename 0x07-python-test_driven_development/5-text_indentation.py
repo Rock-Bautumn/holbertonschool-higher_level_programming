e20763fe329d48907398bfe0f936f590c8a79f2d
Rock-Bautumn <3623@holbertonschool.com>
2022-01-17 22:27:09 -0600

    Adding files for task 5 in 0x07

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,4 +3,16 @@
 This function indents text
 """
 
-"fubar"
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    nl_array = [".", "?", ":"]
+    for i in range(len(text)):
+        if text[i] == " " and text[i - 1] in nl_array:
+            pass
+        else:
+            print(text[i], end="")
+        if text[i] in nl_array:
+            print("\n", end="")
+            print("\n", end="")
