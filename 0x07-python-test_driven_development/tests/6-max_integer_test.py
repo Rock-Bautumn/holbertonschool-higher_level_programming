4de3029b364b4d53cc715979989152e67c1f16c7
Rock-Bautumn <3623@holbertonschool.com>
2022-01-17 23:27:29 -0600

    Adding files for task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,40 @@
 #!/usr/bin/python3
 """
-This function finds the max integer from a list
+Unittesting for 6-max_integer.py - max_integer([list])
 """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-"fubar"
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    Unittest test class for max int testing
+    """
+
+    def test_docstring(self):
+        """Tests for a function docstring"""
+        doc = max_integer.__doc__
+        self.assertTrue(len(doc) > 1)
+
+    def test_empty(self):
+        """Test if the list is empty"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_type(self):
+        """Testing to make sure the data is a valid list"""
+        self.assertIs(list, list)
+        self.assertRaises(TypeError, max_integer((1, 2)))
+
+    def test_one_int(self):
+        self.assertEqual(max_integer([42]), 42)
+
+    def test_positives(self):
+        """Testing only positive integers"""
+        self.assertEqual(max_integer([5, 6, 7, 8]), 8)
+
+    def test_negatives(self):
+        """Testing only negative integers"""
+        self.assertEqual(max_integer([-1, -5, -999999999999]), -1)
+
+if __name__ == "__main__":
+    unittest.main()
