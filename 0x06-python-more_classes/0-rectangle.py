77050faf6ec4f54b4d685846b212bc7aa62f1685
Rock-Bautumn <3623@holbertonschool.com>
2022-01-18 21:43:10 -0600

    Initial commit for 0x06, adding README and task 0

---
+++ 0x06-python-more_classes/0-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+This module has a class for Rectangle
+"""
+
+class Rectangle:
+    """
+    Class for rectangles, has width and height
+    """
+
+
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
+
+
+    @property
+    def width(self):
+        """
+        This is the getter for the width
+        """
+        return self.__width
+
+
+    @property
+    def height(self):
+        """
+        This is the getter for the height
+        """
+        return self.__height
+
+
+    @width.setter
+    def width(self, value):
+        """
+        This is the setter for width
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+    @height.setter
+    def height(self, value):
+        """
+        This is the setter for height
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
---
+++ 0x06-python-more_classes/README.md
@@ -0,0 +1,5 @@
+**0x06-python-more_classes**
+
+*More classes in python*
+
+There's even more to learn about how python implements classes.
