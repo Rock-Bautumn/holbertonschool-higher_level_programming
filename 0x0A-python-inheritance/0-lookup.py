3220ad0145c1f1a89268cdb3ea164f7a41245a52
Rock-Bautumn <3623@holbertonschool.com>
2022-01-20 21:46:34 -0600

    Adding documentation to task 0 in 0x0A

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This function returns a list of available attributes and methods available
+"""
+
 
 def lookup(obj):
+    """
+    Returns the methods and availables available with the object
+    """
     return dir(obj)
