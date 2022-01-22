c902c82c8dbb08217baee696db70071f0a443286
Rock-Bautumn <3623@holbertonschool.com>
2022-01-21 22:43:00 -0600

    Initial commit for 0x0B

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a function to read a file
+"""
+
+def read_file(filename=""):
+    """
+    This reads a file line by line
+    """
+
+    with open(filename) as thefile:
+        for line in thefile:
+            print(line, end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,5 @@
+**0x0B-python-input_output**
+
+*file in, file out*
+
+In this project, we learn about file I/O in python.
