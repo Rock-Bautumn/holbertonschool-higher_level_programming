ef5cb223d4553057efc20959b813688ee0460bd3
Rock-Bautumn <3623@holbertonschool.com>
2022-01-23 19:04:42 -0600

    Typo fix for task 4 in 0x0A

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,6 +9,6 @@ def inherits_from(obj, a_class):
     """
     Returns if object is only a subclass of a_class
     """
-    if type(ob) is a_class:
+    if type(obj) is a_class:
         return False
     return issubclass(type(obj), a_class)
