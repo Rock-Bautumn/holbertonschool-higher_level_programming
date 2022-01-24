31510bd909d2b0c11928ae7742fe9c1c63ad0344
Rock-Bautumn <3623@holbertonschool.com>
2022-01-23 21:32:25 -0600

    Adding task 9 to 0x0A

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module contains a class named BaseGeometry
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This class defines a rectangle
+    """
+    pass
+
+    def __init__(self, width, height):
+        """
+        Initialize the rectangle
+        """
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        This method returns the area of the rectangle
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """
+        Returns info about the rectangle
+        """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
