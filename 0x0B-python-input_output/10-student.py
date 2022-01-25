640faaf72e2d6b6984d05b211503e2384432786d
Rock-Bautumn <3623@holbertonschool.com>
2022-01-24 22:47:46 -0600

    Updating definition for to_json in 10 of 0x0B

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,7 +17,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         this returns the dict of the object
         """
