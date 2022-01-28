c067acef83697a67f49bb7c87303b9bcdd8920e2
Rock-Bautumn <3623@holbertonschool.com>
2022-01-27 20:39:52 -0600

    Adding broken init for square, no y update - 0x0C

---
+++ 0x0C-python-almost_a_circle/10-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 10-main """
+from models.square import Square
+
+if __name__ == "__main__":
+
+    s1 = Square(5, 6, 2222, 89)
+    print(s1)
+    print(s1.size)
+    s1.size = 10
+    print(s1)
+    print(s1.size)
+    # s1.size = -7
+    print(s1)
+    print(s1.size)
+    
+
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,9 @@
 """
 This file contains the square class, which inherits the rectangle class
 """
+from ctypes import sizeof
+
+
 Rectangle = __import__("models.rectangle").rectangle.Rectangle
 validate_int = __import__("models.base").base.validate_int
 
@@ -12,5 +15,29 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize the square
+        """
         # validate_int("size", size)
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+        Returns the value of the size (width)
+        """
+        return self.width
+    
+    @size.setter
+    def size(self, num):
+        """
+        Sets the size of the square, affecting width and height
+        """
+        super().__init__(num, num)
+
+    def __str__(self):
+        """
+        Makes a pretty string of our square object
+        """
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
