221e4ff85fbf55c45497d02427b300f3eb37c7aa
Rock-Bautumn <3623@holbertonschool.com>
2022-02-09 22:34:35 -0600

    Adding task 2 to 0x00 sql

---
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Deletes the database hbtn_0c_0 in your MySQL server
+-- no fail if database does not exist
+-- so basically ensure the database hbtn_0c_0 does not exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
