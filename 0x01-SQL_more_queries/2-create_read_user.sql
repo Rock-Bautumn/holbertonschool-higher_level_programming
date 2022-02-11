94b95848b4e2ced92289a046c18ce10d4c1188ee
Rock-Bautumn <3623@holbertonschool.com>
2022-02-10 21:31:24 -0600

    Adding semicolon to fix mistake, task 2 0x01 sql

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,8 @@
 -- Write a script that creates the database hbtn_0d_2 and the user user_0d_2. 
 
 -- Create the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
 -- Create the user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'hbtn_0d_2_pwd';
 
