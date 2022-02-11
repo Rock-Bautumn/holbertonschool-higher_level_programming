6f6d065c01ce5c564b52786fefee76db89a27d0f
Rock-Bautumn <3623@holbertonschool.com>
2022-02-10 21:55:14 -0600

    switching the order for table and db in 2 0x01 sql

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user
-CREATE USER IF NOT EXISTS `user_0d_2`@'localhost' IDENTIFIED BY 'hbtn_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'hbtn_0d_2_pwd';
 
 -- Set user permissions
-GRANT SELECT ON *.hbtn_0d_2 TO `user_0d_2`@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
