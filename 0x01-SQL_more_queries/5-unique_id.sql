9eb64f55bd50c8b21b536112651646acbb2c0005
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 10:10:09 -0600

    Undoing incorrect changes

--- 0x01-SQL_more_queries/5-unique_id.sql
+++ 0x01-SQL_more_queries/5-unique_id.sql
@@ -1,8 +1,4 @@
 -- Create a table with unique IDs
 
-
--- Make the database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
 -- Make the table
-CREATE TABLE IF NOT EXISTS states (id INT DEFAULT 1 UNIQUE, name VARCHAR(256), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
