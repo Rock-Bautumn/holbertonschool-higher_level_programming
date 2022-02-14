ad8ebbc1892981a8ae9f54c51e1b3f845862eb2f
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 13:25:05 -0600

    Adding task 9 in 0x01 sql

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,7 @@
--- This file does effectively nothing.
+-- Display the cities contained in the database hbtn_0d_usa
 
--- Do nothing
-
-SELECT * FROM title;
+-- Display the cities
+SELECT cities.id, cities.names, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
