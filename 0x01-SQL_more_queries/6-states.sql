ca29f491bc003827c27a904e0cac4dd1a6466024
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 11:09:20 -0600

    Using a different solution for task 6 in 0x01 sql

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -1,11 +1,9 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
-
 -- Make the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
---Make the table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-    id INT NOT NULL AUTO_INCREMENT,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
