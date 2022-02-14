a8d4b2c0061e569b80c55bba3b9a0360f526a07c
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 10:19:19 -0600

    Adding use database in 6 of 0x01 sql

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -3,5 +3,7 @@
 -- Make the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 --Make the table
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
