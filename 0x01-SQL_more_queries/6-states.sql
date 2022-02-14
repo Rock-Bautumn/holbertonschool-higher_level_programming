e2a57858be13224954432c355acba9549607f108
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 10:33:30 -0600

    Removing a space and the USE statement from 6 in 0x01 sql

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -3,10 +3,8 @@
 -- Make the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-USE hbtn_0d_usa;
-
 --Make the table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
