c6e848a031070e805b5f552026bbc4180aa2a13e
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 11:19:36 -0600

    New task 7 in 0x01 sql

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -4,11 +4,10 @@
 
 -- Make the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
---Make the table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    state_id INT NOT NULL, 
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
