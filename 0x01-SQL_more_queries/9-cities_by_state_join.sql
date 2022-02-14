9f275ea843fa1f77e23f0038689e4ae392f1a7ae
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 13:38:14 -0600

    Removed ORDER BY in 9 of 0x01 sql

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,4 @@
 -- Display the cities
 SELECT cities.id, cities.names, states.name 
 FROM cities 
-    INNER JOIN states ON cities.state_id = states.id 
-ORDER BY cities.id;
+    INNER JOIN states ON cities.state_id = states.id;
