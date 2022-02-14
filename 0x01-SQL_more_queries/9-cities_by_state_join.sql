293887cbb1dee16c44842fbde7653522b63b1bb4
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 14:20:55 -0600

    Yet another try for 9 in 0x01

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
--- Display the cities contained in the database hbtn_0d_usa
--- Display the cities
-SELECT cities.id, cities.names, states.name 
-FROM cities 
-INNER JOIN states
-ON cities.state_id = states.id;
+-- List all cities contained in the DB hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+    JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
