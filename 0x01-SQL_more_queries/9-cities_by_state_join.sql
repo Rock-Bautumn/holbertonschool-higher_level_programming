0e88ae1cb10b4741ce15dbee2fdaac51b6cb8c8c
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 13:40:23 -0600

    Removing extra spaces in 9 0x01 sql

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,4 +3,5 @@
 -- Display the cities
 SELECT cities.id, cities.names, states.name 
 FROM cities 
-    INNER JOIN states ON cities.state_id = states.id;
+INNER JOIN states
+ON cities.state_id = states.id;
