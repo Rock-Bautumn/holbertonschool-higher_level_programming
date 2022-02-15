185899877e889da5c8ececc627288215131dd4da
Rock-Bautumn <3623@holbertonschool.com>
2022-02-14 22:21:45 -0600

    Adding task 10 to 0x01 sql

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,8 @@
 -- This file does effectively nothing.
 
--- Do nothing
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 
-SELECT * FROM title;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
