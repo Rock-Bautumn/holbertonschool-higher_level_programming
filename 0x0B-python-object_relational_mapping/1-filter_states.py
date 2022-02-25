9b0ff2934e475b88336b0feb863b73c39f7f5700
Rock-Bautumn <3623@holbertonschool.com>
2022-02-25 13:22:49 -0600

    Adding task 1 in 0x0B ORM

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-List every state in the databasse
+Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa: 
 """
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost", post=3306, user=argv[1], passwd=argv[2],
+        host="localhost", port=3306, user=argv[1], passwd=argv[2],
         database=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
     results = cur.fetchall()
     for item in results:
         print(item)
