0fe9ec54e82c491e53f317b7fdfac85eebe66921
Rock-Bautumn <3623@holbertonschool.com>
2022-03-10 10:13:10 -0600

    Adding task 1 to 0x0C network

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -2,14 +2,13 @@
 """
 This file gets the status of Holberton intranet
 """
-
+from sys import argv
 
 if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
-        html = response.read()
-
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: OK")
+    with urllib.request.urlopen(argv[1]) as response:
+        # headinfo = response.read()
+        # print(headinfo.headers)
+        # print(dir(response))
+        print(response.headers["X-Request-Id"])
+        # print(f"response info:\n {response.info()}")
