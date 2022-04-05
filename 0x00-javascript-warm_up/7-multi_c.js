6327df0d090ea4b6e4d7ca638e24879c55fe5c2e
Rock-Bautumn <3623@holbertonschool.com>
2022-04-04 23:21:19 -0500

    Adding task 7 to 0x00 js

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const myArgs = process.argv.slice(2);
+if (isNaN(parseInt(myArgs[0]))) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  const int = parseInt(myArgs[0]);
+  while (i < int) {
+    console.log('C is fun');
+    i++;
+  }
+}
