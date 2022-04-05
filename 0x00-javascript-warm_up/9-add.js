b215bba9c1f6711f176ac887aa356fdd25c72e72
Rock-Bautumn <3623@holbertonschool.com>
2022-04-04 23:33:01 -0500

    Adding task 9 in 0x00 js

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+function add (a, b) {
+  const sum = a + b;
+  return sum;
+}
+const myArgs = process.argv.slice(2);
+const a = parseInt(myArgs[0]);
+const b = parseInt(myArgs[1]);
+console.log(add(a, b));
