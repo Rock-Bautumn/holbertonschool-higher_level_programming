d0224c61920e0b53c90f3f28e3a2416e2bb4c280
Rock-Bautumn <3623@holbertonschool.com>
2022-04-04 23:25:43 -0500

    Adding task 8 to 0x00 js

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const char = 'X';
+const myArgs = process.argv.slice(2);
+if (isNaN(parseInt(myArgs[0]))) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  const int = parseInt(myArgs[0]);
+  while (i < int) {
+    console.log(char.repeat(int));
+    i++;
+  }
+}
