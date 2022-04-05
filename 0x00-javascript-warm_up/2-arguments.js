d33e3f15d256fb7edaecc1c27b6132903e718318
Rock-Bautumn <3623@holbertonschool.com>
2022-04-04 22:37:39 -0500

    Adding task 2 in 0x00 js

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
+  console.log('No argument');
+} else if (myArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
