f6fc3783f17303cbd92752fcc0c33b3dcad8b947
Rock-Bautumn <3623@holbertonschool.com>
2022-04-04 23:40:32 -0500

    Adding task 10 in 0x00 js

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+function factorial (num) {
+  if ((num == 0) || (isNaN(num))) {
+      return 1;
+  } else {
+      return num * factorial(num - 1);
+  }
+}
+const myArgs = process.argv.slice(2);
+const myNum = parseInt(myArgs[0]);
+console.log(factorial(myNum));
