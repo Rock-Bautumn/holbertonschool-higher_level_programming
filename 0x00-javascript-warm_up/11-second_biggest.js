3622ad05ff0eea86b03b3d345c27e563532ea71f
Rock-Bautumn <3623@holbertonschool.com>
2022-04-04 23:55:26 -0500

    Adding task 11 to 0x00 js

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
+  console.log('0');
+} else if (myArgs.length === 1) {
+  console.log('0');
+} else {
+  const sortedArgs = myArgs.sort(function (a, b) { return b - a; });
+  console.log(sortedArgs[1]);
+}
