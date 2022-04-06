e913d38fa505d595c37ac2d998a3a7e0d4c2b7a8
Rock-Bautumn <3623@holbertonschool.com>
2022-04-06 10:40:36 -0500

    Added task 3 in 0x01 js

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
-module.exports = class Rectangle { };
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    let i = 0
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
+    }
+  }
+};
