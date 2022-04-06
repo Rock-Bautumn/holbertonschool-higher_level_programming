fb379db165db41049eda3eb4a40c40f0262df7b5
Rock-Bautumn <3623@holbertonschool.com>
2022-04-06 12:10:17 -0500

    Adding task 6 in 0x01 js

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
-module.exports = class Rectangle { };
+const middleSquare = require('./5-square');
+
+module.exports = class Square extends middleSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let thischar = 'X';
+    if (c) {
+      thischar = c;
+    }
+    let i = 0;
+    while (i < this.width) {
+      console.log(thischar.repeat(this.width));
+      i++;
+    }
+  }
+};
