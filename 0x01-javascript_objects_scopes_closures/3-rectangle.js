138c5403df922bd2fd16900d9c724e4b9bdf9257
Rock-Bautumn <3623@holbertonschool.com>
2022-04-06 10:43:02 -0500

    semistandard fix for task 3 in 0x01 js

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,8 +6,9 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-  print() {
-    let i = 0
+
+  print () {
+    let i = 0;
     while (i < this.height) {
       console.log('X'.repeat(this.width));
       i++;
