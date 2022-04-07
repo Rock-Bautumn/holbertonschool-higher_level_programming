b5bce07566cba97b8f5c45857e575f4b3f22c8df
Rock-Bautumn <3623@holbertonschool.com>
2022-04-07 13:07:45 -0500

    Adding task 5 in 0x02 js

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const myArgs = process.argv.slice(2);
+const reqRoute = myArgs[0];
+const fileTarget = myArgs[1];
+// console.log(reqRoute);
+request(reqRoute, function (error, response, body) {
+  if (error !== null) {
+    console.error('error:', error); // Print the error if one occurred
+  }
+  else {
+    // console.log('code:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    // console.log(typeof body);
+    // const resJSON = JSON.parse(body);
+    fs.writeFile(fileTarget, body, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
